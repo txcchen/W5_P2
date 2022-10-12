1d9ba77b32e98a4f243e98f0dd2315232bdeea9f
Lesley Chen <lesleychen456@gmail.com>
2022-10-12 02:36:35 -0400

    layout done

---
+++ app/src/main/java/com/example/rating/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.rating;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity implements LeftRightFragment.LRButtons {
+
+    ImageView imgView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        FragmentManager manager= getSupportFragmentManager();
+        FragmentTransaction transaction = manager.beginTransaction();
+
+        ImageRatingFragment imgf = new ImageRatingFragment();
+        transaction.add(R.id.ImgFragFrame, imgf);
+        transaction.commit();
+
+    }
+
+//    @Override
+//    public void buttonClick() {
+//        imgView= (ImageView) ImageRatingFragment.getImgView();
+//        imgView.setImageResource(R.drawable.cat_screech);
+//    }
+}
